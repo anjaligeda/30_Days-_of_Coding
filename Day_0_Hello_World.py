d1f0f90d141d6384c6ab636a087f73ffaf9902dd
Anjali Geda <91786321+anjaligeda@users.noreply.github.com>
2021-12-14 10:32:19 +0530

    Day_0_HelloWorld.py

---
+++ Day_0_Hello_World.py
@@ -0,0 +1,17 @@
+#Day_0_Hello_World
+'''To complete this challenge, you must save a line of input from stdin to a variable, print Hello, World.
+on a single line, and finally print the value of your variable on a second line.
+The instructions are Java-based, but we support submissions in many popular languages. You can switch 
+languages using the drop-down menu above your editor, and the inputString variable may be written
+differently depending on the best-practice conventions of your submission language.'''
+
+#CODE
+input_string=input()
+print('Hello,World.')
+print(input_string)
+
+'''#INPUT
+Welcome to 30 Days of code!
+#OUTPUT
+Hello,World.
+Welcome to 30 Days of code!'''
